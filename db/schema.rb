82468ef7ff38653d4e6aef06c513a89a3c9f1fda
Vaio <BatoDagbaev@CCC.local>
2019-06-06 14:57:14 -0400

    Implemented image defaults for portfolio items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_06_154359) do
+ActiveRecord::Schema.define(version: 2019_06_06_184007) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,8 +21,8 @@ ActiveRecord::Schema.define(version: 2019_06_06_154359) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "slug"
-    t.integer "status", default: 0
     t.bigint "topic_id"
+    t.integer "post_status"
     t.index ["slug"], name: "index_blogs_on_slug", unique: true
     t.index ["topic_id"], name: "index_blogs_on_topic_id"
   end
